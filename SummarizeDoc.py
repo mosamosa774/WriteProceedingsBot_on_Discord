b6b6d252502e0a59f010c8eb7862e10f49e8c56e
mosamosa774 <s1220052@gmail.com>
2019-01-08 05:15:04 +0900

    init

---
+++ SummarizeDoc.py
@@ -0,0 +1,110 @@
+# coding: UTF-8
+# pip install ipykernel
+# pip install mecab-python-windows
+import MeCab as mec
+import sys
+import re
+import math
+
+def analyze(contents, number_of_line, debug):
+    m = mec.Tagger("-Ochasen")
+    texts = re.split('[。\n！？!?]', contents.replace('>',''))
+    words_map = {}
+    num_of_sentence = len(texts)
+    _texts = []
+    for txt in texts:
+        res = m.parse(txt)
+        _texts.append(res)
+        lines = res.split('\n')
+        items = (re.split('[\t,]', line) for line in lines)
+        for item in items:
+            try:
+                top = item[2][0]
+                if(top in words_map):
+                    exist = False
+                    for i in range(len(words_map[top])):
+                        if(item[2] == words_map[top][i][0]):
+                            words_map[top][i] = (item[2],words_map[top][i][1]+1)
+                            exist = True
+                            break
+                    if(not exist):
+                        words_map[top].append((item[2],1))
+                else:
+                    words_map[top] = [ (item[2],1) ] #(word,its number)
+            except:
+                pass
+
+    for i in words_map:
+        for j in range(len(words_map[i])):
+            v = math.log(num_of_sentence / words_map[i][j][1])
+            words_map[i][j] = (words_map[i][j][0],v)
+
+    result = []
+    for i in range(len(_texts)):
+        sum_word = 0
+        lines = _texts[i].split('\n')
+        items = (re.split('[\t,]', line) for line in lines)
+        for item in items:
+            try:
+                top = item[2][0]
+                for j in words_map[top]:
+                    if(j[0] == item[2]):
+                        sum_word += j[1]
+                        break
+            except:
+                pass
+        if(not texts[i] == ''):
+            result.append( (texts[i],sum_word) )
+    if(debug):
+        print("result")
+        for i in result:
+            print(i)
+    ranks = [(-1,-1) for i in range(number_of_line)]
+    for i in range(len(result)):
+        for j in range(len(ranks)):
+            if(ranks[j][1] < result[i][1]):
+                exist = False
+                for k in ranks:
+                    if(not k[0] == -1):
+                        if(result[i][0] == result[k[0]][0]):
+                            exist = True
+                            break
+                if(exist):
+                    break
+
+                if( (not ranks[j][0] == -1) and (j+1) < number_of_line):
+                    tmp = (ranks[j][0],ranks[j][1])
+                    for k in range(j,number_of_line-2):
+                        if(not ranks[k+1][0] == -1):
+                            swap = (ranks[k+1][0],ranks[k+1][1])
+                            ranks[k+1] = tmp
+                            tmp = swap
+                        else:
+                            ranks[k+1] = tmp
+                            break
+                
+                ranks[j] = (i,result[i][1])
+                break
+
+    res = {}
+    for i in ranks:
+        res[i[0]] = result[i[0]][0]
+    res = sorted(res.items(), key=lambda x: x[0])
+    _res = []
+    for i in res:
+        _res.append(i[1])
+    return _res
+
+
+debug = False
+contents = None
+try:
+    with open(sys.argv[1], encoding="utf-8") as f:
+        contents = f.read()
+    if(sys.argv[3] == '-d'):
+        debug = True
+except:
+    pass
+res = analyze(contents, int(sys.argv[2]),debug)
+for i in res:
+    print(i)
---
+++ WriteProceedingsBot.py
@@ -0,0 +1,60 @@
+import asyncio
+import datetime
+import json
+import re
+import os
+import SummarizeDoc as sdoc
+
+with open("token.json", "r") as f:
+    data = f.read()
+tokens = json.loads(data)
+
+BOT_TOKEN = tokens['AccountToken']
+
+import discord
+client = discord.Client()
+
+talking = {}
+talked_content = {}
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print('BOT-NAME :', client.user.name)
+    print('BOT-ID   :', client.user.id)
+    print('------')
+
+@client.event
+async def on_message(message):
+    global talking
+    global talked_content
+    # BOTとメッセージの送り主が同じ人なら処理しない
+    if client.user == message.author:
+        return
+    if message.content == "#talking":
+        talking[message.channel.id] = True
+        await client.send_message(message.channel,"start writing...")
+    elif message.content == "#end":
+        if message.channel.id in talking:
+            if(talking[message.channel.id]):
+                if(not message.channel.id in talked_content):
+                    return
+                del(talking[message.channel.id])
+                await client.send_message(message.channel,"end writing!")
+                await client.send_message(message.channel,"Dealing...")
+                msg = talked_content[message.channel.id]
+                del(talked_content[message.channel.id])
+                summarize = sdoc.analyze(msg,5,False)
+                msg += "Summarized of these proceedings\n"
+                for i in summarize:
+                    msg += i + "\n"
+                with open("out.txt", mode='w') as f:
+                    f.write(msg)
+                await client.send_file(message.channel, "out.txt", content="done!")
+    elif message.channel.id in talking:
+        if(talking[message.channel.id]):
+            if(not message.channel.id in talked_content):
+                talked_content[message.channel.id] = ""
+            talked_content[message.channel.id] += message.author.user + ": " + message.content + "\n"
+
+client.run(BOT_TOKEN)
